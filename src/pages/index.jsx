26514338dc68306dd22739720cdad4bc73b9f5a2
Manuel Gonzalez <manuelg9704@gmail.com>
2024-06-13 10:39:07 -0500

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LandingPage } from '../../@/components/component/landing-page';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default IndexPage;
